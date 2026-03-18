892628f217f801c2e2f1099b411ff1700e941e39
Abhi Shekar <abhishekarpulla@gmail.com>
2026-03-18 10:32:38 +0530

    Update MissingNumber.java

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -32,3 +32,45 @@ class MissingNumber {
     }
 }
 
+/*
+Approach: Cyclic Sort
+
+Place each number at index = value
+
+Then find index where nums[i] != i
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class MissingNumberCyclic {
+
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+        int i = 0;
+
+        while(i < n){
+
+            if(nums[i] < n && nums[i] != nums[nums[i]]){
+
+                int correctIndex = nums[i];
+
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+
+        for(i = 0; i < n; i++){
+            if(nums[i] != i){
+                return i;
+            }
+        }
+
+        return n;
+    }
+}
